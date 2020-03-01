882844a9fd3fc8e29efad6cc176a3c080398e9d7
Kirzahk <61593550+Kirzahk@users.noreply.github.com>
2020-03-01 20:21:20 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Ejemplo_007___intro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Escribe tu nombre y presiona INTRO: ");
+            string Nombre = Console.ReadLine();
+            Console.Write("Escribe tu edad y presiona INTRO: ");
+            string edad = Console.ReadLine();
+            Console.WriteLine($"Hola {Nombre}, tienes buena pinta para tu edad de {edad} años.");
+        }
+    }
+}
